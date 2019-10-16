2093c95b518a770214149bdfb5d00f23c2f6ae79
Anoop Lekshmanan <anoop.lekshmanan1@t-mobile.com>
2019-10-16 18:32:11 +0400

    Description for cluster command

--- cmd/cluster.go
+++ cmd/cluster.go
@@ -24,13 +24,9 @@ import (
 // clusterCmd represents the cluster command
 var clusterCmd = &cobra.Command{
 	Use:   "cluster",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Create an AKS cluster",
+	Long: `Create an AKS cluster, it would use a Random Name for cluster.
+	If you need to specify name or other resources use cluster.yaml file for more custom configuration`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("cluster called")
 	},
