7b651f32b7a0ca5cfc98f5d0d4b21ef450c83937
Anoop Lekshmanan <anoop.lekshmanan1@t-mobile.com>
2019-10-16 14:54:31 +0400

    Add help information

--- cmd/root.go
+++ cmd/root.go
@@ -32,13 +32,10 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
   Use:   "aksctl",
-  Short: "A brief description of your application",
-  Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+  Short: "CLI to easily create cluster on Azure AKS",
+  Long: `aksctl can create a cluster with single command aksctl create cluster
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+  It's written in Go and uses Terraform for the Azure resource creation`,
   // Uncomment the following line if your bare application
   // has an action associated with it:
   //	Run: func(cmd *cobra.Command, args []string) { },
