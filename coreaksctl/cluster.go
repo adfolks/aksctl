a1dbe67d11683b8d78b2959a44d6ad938976b17d
ASHMIL HUSSAIN K <ashmilhussain@gmail.com>
2019-11-20 16:39:24 +0400

    added file flag to create cluster, reading optional flags from yaml fixed and resolved issu Kube config is not set

--- coreaksctl/cluster.go
+++ coreaksctl/cluster.go
@@ -6,21 +6,22 @@ import (
 	"os/exec"
 )
 
-func CreateCluster(clusterName string, resourceGroupName string) {
+func CreateCluster(clusterName string, resourceGroupName string, extraflags []string) {
 	fmt.Println("Starting to set up your k8s Cluster")
 	fmt.Println("This would take a few minutes...")
 	fmt.Println("---------------------------------")
 	//Create AKS Cluster
-	cmd := exec.Command("az", "aks", "create", "--name", clusterName,
-		"--resource-group", resourceGroupName, "--node-count",
-		"2", "--generate-ssh-keys")
+	var args = []string{"aks", "create", "--name", clusterName, "--resource-group", resourceGroupName}
+	args = append(args, extraflags...)
+	fmt.Println(args)
+	cmd := exec.Command("az", args...)
 	var out bytes.Buffer
 	var stderr bytes.Buffer
 	cmd.Stdout = &out
 	cmd.Stderr = &stderr
 	err := cmd.Run()
 	if err != nil {
-		fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
+		fmt.Println("Failed :" + fmt.Sprint(err) + ": " + stderr.String())
 		return
 	}
 	fmt.Println("Result: " + out.String())
